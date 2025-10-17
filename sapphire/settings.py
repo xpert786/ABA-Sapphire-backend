b9b9dc0db00f4373875866200606812795920c31
Arun Rana <m.xpert786@gmail.com>
2025-10-17 14:13:34 +0530

    update settings

--- sapphire/settings.py
+++ sapphire/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/sapphire/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
  
 
 # Media files
